05f1a990d77b646524a38cb9826319230f90498d
MirageM <imiragesz@gmail.com>
2023-07-06 17:47:57 -0400

    permuate

---
+++ permute.py
@@ -0,0 +1,9 @@
+# LeetCode Problem: 46. Permutations
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        
