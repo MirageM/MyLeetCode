7694c15ed07aec62c10c75cdd4571170263025f7
MirageM <imiragesz@gmail.com>
2023-07-07 19:37:14 -0400

    2D Array

--- rotateImage.py
+++ rotateImage.py
@@ -5,3 +5,15 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+#         # Solution 1: 2D array
+#         # Time: O(n^2)
+#         # Space: O(n^2)
+        n = len(matrix)
+        res = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                res[j][n - i - 1] = matrix[i][j]
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = res[i][j]
+        return matrix
