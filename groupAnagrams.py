86aff65a17fbc7f324414440c49ca16aaec1afa2
MirageM <imiragesz@gmail.com>
2023-07-08 14:30:13 -0400

    strs_table[sorted_string].append(string)

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -5,3 +5,10 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
+        strs_table = {}
+        for string in strs:
+            sorted_string = ''.join(sorted(string))
+            if sorted_string not in strs_table:
+                strs_table[sorted_string] = []
+            strs_table[sorted_string].append(string)
+        return list(strs_table.values())
