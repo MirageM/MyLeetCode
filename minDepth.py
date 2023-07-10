ef3b923a5b2b964628d5f2fe3a0a2ddb8e1e6161
MirageM <imiragesz@gmail.com>
2023-07-10 16:11:28 -0400

    node.left is None and node.right is None

---
+++ minDepth.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        queue = [root]
+        depth = 1
+        rightMost = root
+        while len(queue) > 0:
+            node = queue.pop(0)
+            if node.left is None and node.right is None:
+                break
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+            if node == rightMost:
+                depth += 1
+                if node.left is not None:
+                    rightMost = node.left
+                else:
+                    rightMost = node.right
+        return depth
+
+                    
