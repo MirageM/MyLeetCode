8addfcf93b8f27e6c202c0daaa22f0931eeb57f5
MirageM <imiragesz@gmail.com>
2023-07-17 22:21:30 -0400

    nums[i], nums[zero] = nums[zero], nums[i]

--- moveZeroes.py
+++ moveZeroes.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        if 0 not in nums:
+            return nums
+        zero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
+
+    
