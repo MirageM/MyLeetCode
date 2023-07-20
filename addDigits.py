a8b8920b4b2f0f19420a78cd310e1d2444b11c3c
MirageM <imiragesz@gmail.com>
2023-07-20 18:18:33 -0400

    return num % 9

--- addDigits.py
+++ addDigits.py
@@ -1,4 +1,20 @@
 # Leetcode problem: 258. Add Digits
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        else:
+            return num % 9
+
+
+
 # Time Complexity: O(n) Space Complexity: O(1)
 class Solution(object):
     def addDigits(self, num):
