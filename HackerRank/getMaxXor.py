4b564738311dc5df28daf8c5ec6628b946f180d0
MirageM <imiragesz@gmail.com>
2023-07-29 22:13:25 -0400

    getMaxXor(n): num_bits = len(bin(n)) - 2 max_xor = (1 << num_bits) - 1

---
+++ HackerRank/getMaxXor.py
@@ -0,0 +1,10 @@
+def getMaxXor(n):
+	num_bits = len(bin(n)) - 2
+	max_xor = (1 << num_bits) - 1
+	return max_xor - 1
+
+
+def getMaxXor(n):
+	num_bits = len(bin(n)) - 2
+	max_xor = (1 << num_bits) - 1
+	return max_xor - 1
