82ce87acac7d8f4829667f11d0f6b7b3da09f6e6
MirageM <imiragesz@gmail.com>
2023-07-29 22:22:51 -0400

    Time Complexity: O(1) Space Complexity: O(1)

--- HackerRank/getMaxXor.py
+++ HackerRank/getMaxXor.py
@@ -1,5 +1,6 @@
 # https://www.hackerrank.com/challenges/maximizing-xor/problem
 # Maximizing XOR
+# Time Complexity: O(1) Space Complexity: O(1)
 def getMaxXor(n):
 	num_bits = len(bin(n)) - 2
 	max_xor = (1 << num_bits) - 1
