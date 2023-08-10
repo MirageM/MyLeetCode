7869f31f37733f13feaef52f580a7d3d5edf1d10
MirageM <imiragesz@gmail.com>
2023-08-10 16:50:14 -0400

    ans = ans + nums[i] ** 2

--- sumOfSquares.py
+++ sumOfSquares.py
@@ -4,3 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            if n%(i+1) == 0:
+                ans = ans + nums[i]**2
+        return ans
