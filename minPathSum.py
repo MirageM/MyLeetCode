6b6c278815e410bb3f09f5fe31efa3742985de15
MirageM <imiragesz@gmail.com>
2023-08-09 21:34:19 -0400

    theMaximumAchievableX(num, t): type num int, type t: int, rtype: int

--- minPathSum.py
+++ minPathSum.py
@@ -1,5 +1,3 @@
-# Leetcode 64: Minimum Path Sum
-# Time Complexity: O(m*n) Space Complexity: O(1)
 class Solution(object):
     def minPathSum(self, grid):
         """
@@ -9,11 +7,13 @@ class Solution(object):
         m = len(grid)
         n = len(grid[0])
 
-        for i in range(1, m):
-            grid[i][0] += grid[i-1][0]
-        for j in range(1, n):
-            grid[0][j] += grid[0][j-1]
-        for i in range(1, m):
-            for j in range(1, n):
+        for i in range(m):
+            grid[i][0] = grid[i-1][0]
+
+        for j in range(n):
+            grid[0][j] = grid[0][j-1]
+
+        for i in range(m):
+            for j in range(n):
                 grid[i][j] += min(grid[i-1][j], grid[i][j-1])
         return grid[m-1][n-1]
