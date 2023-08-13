c7d52363d3d819a412e92a54cba34809ec9b03b6
MirageM <imiragesz@gmail.com>
2023-08-13 11:02:48 -0400

    max_by_digit[digit] = max(max_by_digit[digit], num)

--- maxSum.py
+++ maxSum.py
@@ -4,3 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        max_by_digit = defaultdict(int)
+        max_sum = -1
+        for num in nums:
+            digit = max(str(num))
+            if digit in max_by_digit:
+                max_sum = max(max_sum, max_by_digit[digit] + num)
+            max_by_digit[digit] = max(max_by_digit[digit], num)
+        return max_sum
