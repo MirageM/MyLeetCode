70439bd90bc840dce81772fc4d9c943605de867e
MirageM <imiragesz@gmail.com>
2023-08-14 18:24:42 -0400

    kItemsWithMaximumSum numOnes: int numZeros: int numNegOnes: int k: int rtype: int

---
+++ kItemsWithMaximumSum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kItemsWithMaximumSum(self, numOnes, numZeros, numNegOnes, k):
+        """
+        :type numOnes: int
+        :type numZeros: int
+        :type numNegOnes: int
+        :type k: int
+        :rtype: int
+        """
