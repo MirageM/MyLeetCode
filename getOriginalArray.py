ebe27d05d82a8cb857257687c1433c1fe3105214
MirageM <imiragesz@gmail.com>
2023-08-15 17:24:56 -0400

    getOrginalArray(pref) arr.append(pref[i])

---
+++ getOriginalArray.py
@@ -0,0 +1,404 @@
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = [] 
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOrignalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0: 
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] 6 pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
+
+def getOriginalArray(pref):
+	arr = []
+	for i in range(len(pref)):
+		if i == 0:
+			arr.append(pref[i])
+		else:
+			arr.append(pref[i] ^ pref[i-1])
+	return arr
