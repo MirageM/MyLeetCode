bf767f1cefad19ae14b6602c1ee78393c10b4809
MirageM <imiragesz@gmail.com>
2023-08-17 17:53:12 -0400

    concatenation += int(str(nums[0]) + str(nums[-1]))

--- findTheArrayConcVal.py
+++ findTheArrayConcVal.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        concatenation = 0
+        while len(nums) > 0:
+            if len(nums) > 1:
+                concatenation += int(str(nums[0]) +str(nums[-1]))
+                del nums[-1]
+            else:
+                concatenation += nums[0]
+            del nums[0]
+        return concatenation 
