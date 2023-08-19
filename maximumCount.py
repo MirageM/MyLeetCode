f74feb44dba27885fdc66ecd4c58dbd095ebef41
MirageM <imiragesz@gmail.com>
2023-08-18 22:45:58 -0400

    if nums[i] > 0: pos = total_len - i

--- maximumCount.py
+++ maximumCount.py
@@ -4,3 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        total_len = len(nums)
+        pos, neg = 0, 0
+        zero_found = False
+        if nums[-1] < 0:
+            return total_len
+        for i in range(total_len):
+            if nums[i] > 0:
+                pos = total_len - i
+                if neg == 0:
+                    neg = i
+                break
+            if nums[i] == 0 and not zero_found:
+                neg, zero_found = i, True
+        return max(pos, neg)
