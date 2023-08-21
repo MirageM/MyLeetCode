3e3322f5efd513d3eacbb84bfc8087413fa073ff
MirageM <imiragesz@gmail.com>
2023-08-20 23:33:09 -0400

    if i%2==1 and s[i] == '1' odd += 1

--- evenOddBit.py
+++ evenOddBit.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
+        s = bin(n)[2:][::-1]
+        even = 0
+        odd = 0
+        for i in range(len(s)):
+            if i%2==1 and s[i] == '1':
+                odd += 1
+            if i%2==0 and s[i] == '1':
+                even += 1
+        return [even, odd]
