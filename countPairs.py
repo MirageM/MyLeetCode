ad6e80e3485cc0dfee8d5f782c4cedd051ca12b9
MirageM <imiragesz@gmail.com>
2023-08-22 22:00:57 -0400

    nums[left] + nums[right] < target: count += right - left

--- countPairs.py
+++ countPairs.py
@@ -5,3 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        while left < right:
+            if nums[left] + nums[right] < target:
+                count += right - left
+                left += 1
+            else:
+                right -= 1
+        return count
