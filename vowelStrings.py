f6c6ae38e2a00d06293b706761342b082afc2b60
MirageM <imiragesz@gmail.com>
2023-08-26 21:20:54 -0400

    vowelStrings: words: List[str], left: int, right: int, rtype:int

---
+++ vowelStrings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def vowelStrings(self, words, left, right):
+        """
+        :type words: List[str]
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
