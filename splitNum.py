bfc0f147f3b0024d9d9dafdfab0c4ba8013dff12
MirageM <imiragesz@gmail.com>
2023-08-27 22:52:21 -0400

    num1 += str(pq.get()) if not pq.empty(): num2 += str(pq.get())

--- splitNum.py
+++ splitNum.py
@@ -1,6 +1,22 @@
+from queue import PriorityQueue
+
 class Solution(object):
     def splitNum(self, num):
         """
         :type num: int
         :rtype: int
         """
+        a = [int(x) for x in str(num)]
+        num1 = ""
+        num2 = ""
+        pq = PriorityQueue()
+        for i in a:
+            pq.put(i)
+        while not pq.empty():
+            num1 += str(pq.get())
+            if not pq.empty():
+                num2 += str(pq.get())
+        if num2:
+            return int(num1) + int(num2)
+        else:
+            return int(num1)
